cc8cfbecf1f149e2fd9f19b24bd03302e6ea37bc
c0a22001 <c0a2200113@edu.teu.ac.jp>
2023-11-28 14:16:27 +0900

    爆弾を表示

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -9,8 +10,15 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex02/fig/3.png")#surface
+    kk_img = pg.image.load("ex02/fig/3.png")  # surface
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bb_img = pg.surface((20, 20))  # 練習1 surfaceを作る
+    bb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -18,8 +26,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
-        screen.blit(bg_img, [0, 0])#スクリーンに張り付ける
-        screen.blit(kk_img, [900, 400])#上から順に表示される
+        screen.blit(bg_img, [0, 0])  # スクリーンに張り付ける
+        screen.blit(kk_img, [900, 400])  # 上から順に表示される
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
