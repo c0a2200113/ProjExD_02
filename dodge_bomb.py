154b08b43111d62f2e4fafcc0ac98694d86c307e
c0a22001 <c0a2200113@edu.teu.ac.jp>
2023-11-28 16:00:03 +0900

    追加機能1:こうかとん回転

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -31,17 +31,27 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex02/fig/3.png")  # surface
+    kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rct = kk_img.get_rect()  # 練習3 こうかとんsurface
+    kk_flip = pg.transform.flip(kk_img, True, False)
+    kk_img0 = pg.transform.rotozoom(kk_flip, 90, 1.0)
+    kk_img1 = pg.transform.rotozoom(kk_flip, 45, 1.0)
+    kk_img2 = kk_flip
+    kk_img3 = pg.transform.rotozoom(kk_flip, -45, 1.0)
+    kk_img4 = pg.transform.rotozoom(kk_flip, -90, 1.0)
+    kk_img5 = pg.transform.rotozoom(kk_img, 45, 1.0)
+    kk_img6 = kk_img
+    kk_img7 = pg.transform.rotozoom(kk_img, -45, 1.0)
+    kk_imgs = [kk_img0, kk_img1, kk_img2, kk_img3, kk_img4, kk_img5, kk_img6, kk_img7]  # 追加機能1
+    kk_rct = kk_img.get_rect()  # 練習3:こうかとんsurface
     kk_rct.center = 900, 400
-    bb_img = pg.Surface((20, 20))  # 練習1 surfaceを作る
+    bb_img = pg.Surface((20, 20))  # 練習1:surfaceを作る
     bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
-    vx, vy = +5, +5  # 練習2 爆弾の速度
+    vx, vy = +5, +5  # 練習2:爆弾の速度
 
     clock = pg.time.Clock()
     tmr = 0
@@ -50,7 +60,7 @@ def main():
             if event.type == pg.QUIT:
                 return
             
-        if kk_rct.colliderect(bb_rct):  # 練習5 衝突判定　bbとkk逆でもいい
+        if kk_rct.colliderect(bb_rct):  # 練習5:衝突判定(bbとkk逆でもいい)
             print("Game Over")
             return
         
@@ -66,12 +76,29 @@ def main():
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)  # 練習3　こうかとんを移動
-        bb_rct.move_ip(vx, vy)  # 練習2　爆弾を移動
+        
+        if sum_mv[0] == 0 and sum_mv[1] == -5:  # 追加機能1:こうかとんの向き変更
+            screen.blit(kk_imgs[0], kk_rct)
+        elif sum_mv[0] == +5 and sum_mv[1] == -5:
+            screen.blit(kk_imgs[1], kk_rct)
+        elif sum_mv[0] == +5 and sum_mv[1] == 0:
+            screen.blit(kk_imgs[2], kk_rct)
+        elif sum_mv[0] == +5 and sum_mv[1] == +5:
+            screen.blit(kk_imgs[3], kk_rct)
+        elif sum_mv[0] == 0 and sum_mv[1] == +5:
+            screen.blit(kk_imgs[4], kk_rct)
+        elif sum_mv[0] == -5 and sum_mv[1] == +5:
+            screen.blit(kk_imgs[5], kk_rct)
+        elif sum_mv[0] == -5 and sum_mv[1] == -5:
+            screen.blit(kk_imgs[7], kk_rct)
+        else:
+            screen.blit(kk_img, kk_rct)  # 練習3:こうかとんを移動
+            
+        bb_rct.move_ip(vx, vy)  # 練習2:爆弾を移動
         yoko, tate =  check_bound(bb_rct)
-        if not yoko:  #  横方向にはみ出たとき
+        if not yoko:  # 横方向にはみ出たとき
             vx *= -1
-        if not tate:  #  縦方向にはみ出たとき
+        if not tate:  # 縦方向にはみ出たとき
             vy *= -1
         bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
